b5c72b5449de50962720c354316476ae0c6cae28
Lucas Munaro <48440014+Munaro@users.noreply.github.com>
2023-01-07 20:22:22 -0400

    Update top-languages-fetcher.js

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -22,10 +22,10 @@ const fetcher = (variables, token) => {
       query userInfo($login: String!) {
         user(login: $login) {
           # fetch only owner repos & not forks
-          repositories(ownerAffiliations: OWNER, isFork: true, first: 1000) {
+          repositories(ownerAffiliations: OWNER, isFork: true, first: 100) {
             nodes {
               name
-              languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
+              languages(first: 1000, orderBy: {field: SIZE, direction: DESC}) {
                 edges {
                   size
                   node {
